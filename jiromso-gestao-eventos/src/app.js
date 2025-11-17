2539e172946d957faff1aa8cb3ab0f545304dc8b
Gabriel <gabrieldaluz918@gmail.com>
2025-11-17 16:29:59 -0300

    Front end e mudanças no app.js

--- jiromso-gestao-eventos/src/app.js
+++ jiromso-gestao-eventos/src/app.js
@@ -1,13 +1,20 @@
-// Força a inicialização do banco de dados ao carregar o app
-require('./models/usuarioModel');
-require('./models/eventoModel');
+const path = require('path');
+
+require('./models/usuarioModel'); 
+require('./models/eventoModel'); 
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
 app.use(cors());
+
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
 app.use(express.json());
+
 
 const eventoRoutes = require('./routes/eventoRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
@@ -15,14 +22,14 @@ const usuarioRoutes = require('./routes/usuarioRoutes');
 app.use('/api/eventos', eventoRoutes);
 app.use('/api/usuarios', usuarioRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API de Gestão de Eventos "Jiromso" funcionando!');
+
+app.get('/api/health-check', (req, res) => {
+    res.status(200).json({ status: 'ok' });
 });
 
 
--
-  app.get('/api/health-check', (req, res) => {
-    res.status(200).json({ status: 'ok' });
-  });
+/*app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});*/
 
 module.exports = app;
